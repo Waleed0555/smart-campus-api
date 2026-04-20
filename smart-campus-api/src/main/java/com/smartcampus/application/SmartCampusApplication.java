b3e891bce721f08bdc31650582d9a84f29763f19
Waleed0555 <wdustgir@gmail.com>
2026-04-21 00:08:29 +0100

    Add files via upload
    f

---
+++ smart-campus-api/src/main/java/com/smartcampus/application/SmartCampusApplication.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.application;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+// leaving this empty so jersey picks up all the classes on its own
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+
+}
